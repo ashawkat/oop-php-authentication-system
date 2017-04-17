2b22af9880bd7d705583ca8cc2c277a9754e513e
MRQUDUS <MRQUDUS>
2017-04-17 13:35:27 +0100

    Fixed the class.user.php file. Replaced mysql functions with mysqli functions

--- include/class.user.php
+++ include/class.user.php
@@ -1,70 +1,92 @@
 <?php 
 	include "db_config.php";
-
 	class User{
-
+		protected $db;
 		public function __construct(){
-			$db = new DB_con();
+			$this->db = new DB_con();
+			$this->db = $this->db->ret_obj();
 		}
-
+		
 		/*** for registration process ***/
+		
 		public function reg_user($name,$username,$password,$email){
 			//echo "k";
 			
 			$password = md5($password);
 
 			//checking if the username or email is available in db
-			$check = mysql_query("SELECT * FROM users WHERE uname='$username' OR uemail='$email'") or die(mysql_error());
-			//echo "k1";
-			$count_row = mysql_num_rows($check);
-
-			//if the username is not in db then insert to the table
-			if ($count_row == 0){//echo "k3";
-				$result = mysql_query("INSERT INTO users SET uname='$username', upass='$password', fullname='$name', uemail='$email'") or die(mysql_error());
-        		//echo "k4";
-        		return $result;
-			}
-			else { return false;}
-		}
-
-
-		/*** for login process ***/
-		public function check_login($emailusername, $password){
-        	
-        	$password = md5($password);
+			$query = "SELECT * FROM users WHERE uname='$username' OR uemail='$email'";
 			
-			//checking if the username is available in the table
-        	$result = mysql_query("SELECT uid from users WHERE uemail='$emailusername' or uname='$emailusername' and upass='$password'");
-        	$user_data = mysql_fetch_array($result);
-        	$count_row = mysql_num_rows($result);
+			$result = $this->db->query($query) or die($this->db->error);
+			
+			$count_row = $result->num_rows;
+			
+			//if the username is not in db then insert to the table
+			
+			if($count_row == 0){
+				$query = "INSERT INTO users SET uname='$username', upass='$password', fullname='$name', uemail='$email'";
+				
+				$result = $this->db->query($query) or die($this->db->error);
+				
+				return true;
+			}
+			else{return false;}
+			
+			
+			}
+			
+			
+	/*** for login process ***/
+		public function check_login($emailusername, $password){
+        $password = md5($password);
 		
-	        if ($count_row == 1) {
+		$query = "SELECT uid from users WHERE uemail='$emailusername' or uname='$emailusername' and upass='$password'";
+		
+		$result = $this->db->query($query) or die($this->db->error);
+
+		
+		$user_data = $result->fetch_array(MYSQLI_ASSOC);
+		$count_row = $result->num_rows;
+		
+		if ($count_row == 1) {
 	            $_SESSION['login'] = true; // this login var will use for the session thing
 	            $_SESSION['uid'] = $user_data['uid'];
 	            return true;
 	        }
-	        else{
-			    return false;
-			}
-    	}
-
-    	/*** for showing the username or fullname ***/
-    	public function get_fullname($uid){
-	        $result = mysql_query("SELECT fullname FROM users WHERE uid = $uid");
-	        $user_data = mysql_fetch_array($result);
-	        echo $user_data['fullname'];
-    	}
-  
-    	/*** starting the session ***/
-	    public function get_session(){
-		    
-	        return $_SESSION['login'];
-	    }
-
-	    public function user_logout() {
-	        $_SESSION['login'] = FALSE;
-	        session_destroy();
-	    }
+			
+		else{return false;}
+		
 
 	}
-?>
+	
+	
+	public function get_fullname($uid){
+		$query = "SELECT fullname FROM users WHERE uid = $uid";
+		
+		$result = $this->db->query($query) or die($this->db->error);
+		
+		$user_data = $result->fetch_array(MYSQLI_ASSOC);
+		echo $user_data['fullname'];
+		
+	}
+	
+	/*** starting the session ***/
+	public function get_session(){
+	    return $_SESSION['login'];
+	    }
+
+	public function user_logout() {
+	    $_SESSION['login'] = FALSE;
+		unset($_SESSION);
+	    session_destroy();
+	    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- include/db_config.php
+++ include/db_config.php
@@ -1,12 +1,11 @@
 <?php
-//include("dBug.php");
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'oop_log_mod');
 
 class DB_con {
-	
+	public $connection;
 	function __construct(){
 		@$this->connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_DATABASE);
 		
