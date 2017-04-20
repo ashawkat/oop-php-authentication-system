e877939aecaee976d195d7dff09cafe9f23c82bc
Adetona <adetonaabiodun12@gmail.com>
2017-04-19 20:53:26 -0700

    minor changes

--- include/db_config.php
+++ include/db_config.php
@@ -7,7 +7,7 @@ define('DB_DATABASE', 'oop_log_mod');
 class DB_con {
 	public $connection;
 	function __construct(){
-		@$this->connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_DATABASE);
+		$this->connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_DATABASE);
 		
 		if ($this->connection->connect_error) die('Database error -> ' . $this->connection->connect_error);
 		
